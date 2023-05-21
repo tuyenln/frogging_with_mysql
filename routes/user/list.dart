ec825140adae8ca18d3cf638ace82175e8ec1707
tuyenln <tuyenlnse@gmail.com>
2023-05-21 13:31:30 +0700

    update

---
+++ routes/user/list.dart
@@ -0,0 +1,36 @@
+import 'package:dart_frog/dart_frog.dart';
+import 'package:frogging/source/data_source.dart';
+import 'package:frogging/source/package_source.dart';
+import 'package:test/expect.dart';
+
+// we will create a request to read our dataSource
+Future<Response> onRequest(RequestContext context) async {
+  if (context.request.method.value != 'GET') {
+    return Response.json(body: {
+      'code': 400,
+      'status': 'fail',
+      'message': 'Method do not allow'
+    });
+  }
+  // reading the context of our dataSource
+  // final request = context.request;
+  // final body = await request.json();
+
+  // final id = body['id'].toString();
+  // final name = body['name'].toString();
+  // final description = body['description'].toString();
+  // final price = body['price'].toString();
+
+  final dataRepository = context.read<DataSource>();
+  // based on that we will await and fetch the fields from our database
+  final users = await dataRepository.fetchUsers();
+
+  if (users.isEmpty) {
+    return Response.json(
+        body: {'status': 'fail', 'code': 200, 'message': 'fail'});
+  }
+
+  // than we will return the response as JSON
+  return Response.json(
+      body: {'status': 'ok', 'code': 200, 'message': 'ok', 'data': users});
+}
